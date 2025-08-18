849cb91930b9e4af033f5488502c72dc12d1108d
barkesh <dev@barkesh.de>
2025-08-18 08:26:45 +0200

    Practice task (Exprince textarea) added

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,7 +6,7 @@ function submitFeedback () {
  const designation = document.getElementById('designation').value;
  const productType = document.getElementById('productType').value;
  const feedback = document.getElementById('feedbackText').value;
- const submitButton  = document.getElementById('submitBtn');
+ const exprience = document.getElementById('exprience').value;
 
  document.getElementById('userName').innerHTML = username;
  document.getElementById('userAge').innerHTML = age;
@@ -15,10 +15,10 @@ function submitFeedback () {
  document.getElementById('userDesignation').innerHTML = designation;
  document.getElementById('userProductChoice').innerHTML = productType;
  document.getElementById('userFeedback').innerHTML = feedback;
+ document.getElementById('userExprience').innerHTML = exprience;
  
  // Make the user info div visible
  document.getElementById('userInfo').style.display = 'block';
-
  alert('Thank you for your valuable feedback.');
 }
 
